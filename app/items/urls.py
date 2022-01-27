ba5cacc90339a478b1eae721a0f8038aec65ad37
Ryan D Reyes <ryan.reyes2012@gmail.com>
2022-01-27 14:06:30 -0700

    items migration created

--- app/items/urls.py
+++ app/items/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 urlpatterns = [
     path('api/items/index/', item_views.index),
     # path('', item_views.index.as_view(), name='items'),
-    path('api/items/', item_views.bundle_list),
-    path('api/items/<int:pk>/', item_views.bundle_detail),
-    path('api/items/open/', item_views.bundle_list_open)
+    path('api/items/', item_views.item_list),
+    path('api/items/<int:pk>/', item_views.item_detail),
+    path('api/items/open/', item_views.item_list_open)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
