89028f36e25f6c85b0e0a2e7efedd4524358f98e
Daniel Bonner <daniel@legacyretail.com>
2022-01-29 08:33:04 -0600

    finish tasks

--- app/tasks/views.py
+++ app/tasks/views.py
@@ -6,8 +6,8 @@ from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from issues.models import Issue
-from issues.serializers import IssueSerializer
+from tasks.models import Task
+from tasks.serializers import TaskSerializer
 from rest_framework.decorators import api_view
 
 # Create your views here.
@@ -17,55 +17,55 @@ from rest_framework.decorators import api_view
 
 def index(request):
     print("------------------------- I AM HERE")
-    queryset = Issue.objects.all()
-    return render(request, "issues/index.html", {'issues': queryset})
+    queryset = Task.objects.all()
+    return render(request, "tasks/index.html", {'tasks': queryset})
 
 
 """ 
 class index(APIView):
     renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'issues/index.html'
+    template_name = 'tasks/index.html'
 
     def get(self, request):
-        queryset = Issue.objects.all()
-        return Response({'issues': queryset})
+        queryset = Task.objects.all()
+        return Response({'tasks': queryset})
 
 
-class list_all_issues(APIView):
+class list_all_tasks(APIView):
     renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'issues/issue_list.html'
+    template_name = 'tasks/task_list.html'
 
     def get(self, request):
-        queryset = Issue.objects.all()
-        return Response({'issues': queryset}) """
+        queryset = Task.objects.all()
+        return Response({'tasks': queryset}) """
 
 
 # Create your views here.
 @api_view(['GET', 'POST', 'DELETE'])
-def issue_list(request):
+def task_list(request):
     if request.method == 'GET':
-        issues = Issue.objects.all()
+        tasks = Task.objects.all()
 
-        issue_id = request.GET.get('issue id', None)
-        if issue_id is not None:
-            issues = issues.filter(issue_id__icontains=issue_id)
+        task_id = request.GET.get('task id', None)
+        if task_id is not None:
+            tasks = tasks.filter(task_id__icontains=task_id)
 
-        issues_serializer = IssueSerializer(issues, many=True)
-        return JsonResponse(issues_serializer.data, safe=False)
+        tasks_serializer = TaskSerializer(tasks, many=True)
+        return JsonResponse(tasks_serializer.data, safe=False)
         # 'safe=False' for objects serialization
 
     elif request.method == 'POST':
-        issue_data = JSONParser().parse(request)
-        issue_serializer = IssueSerializer(data=issue_data)
-        if issue_serializer.is_valid():
-            issue_serializer.save()
-            return JsonResponse(issue_serializer.data,
+        task_data = JSONParser().parse(request)
+        task_serializer = TaskSerializer(data=task_data)
+        if task_serializer.is_valid():
+            task_serializer.save()
+            return JsonResponse(task_serializer.data,
                                 status=status.HTTP_201_CREATED)
-        return JsonResponse(issue_serializer.errors,
+        return JsonResponse(task_serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
-        count = Issue.objects.all().delete()
+        count = Task.objects.all().delete()
         return JsonResponse(
             {
                 'message':
@@ -75,36 +75,36 @@ def issue_list(request):
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def issue_detail(request, pk):
+def task_detail(request, pk):
     try:
-        issue = Issue.objects.get(pk=pk)
-    except Issue.DoesNotExist:
-        return JsonResponse({'message': 'The issue does not exist'},
+        task = Task.objects.get(pk=pk)
+    except Task.DoesNotExist:
+        return JsonResponse({'message': 'The task does not exist'},
                             status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        issue_serializer = IssueSerializer(issue)
-        return JsonResponse(issue_serializer.data)
+        task_serializer = TaskSerializer(task)
+        return JsonResponse(task_serializer.data)
 
     elif request.method == 'PUT':
-        issue_data = JSONParser().parse(request)
-        issue_serializer = IssueSerializer(issue, data=issue_data)
-        if issue_serializer.is_valid():
-            issue_serializer.save()
-            return JsonResponse(issue_serializer.data)
-        return JsonResponse(issue_serializer.errors,
+        task_data = JSONParser().parse(request)
+        task_serializer = TaskSerializer(task, data=task_data)
+        if task_serializer.is_valid():
+            task_serializer.save()
+            return JsonResponse(task_serializer.data)
+        return JsonResponse(task_serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
-        issue.delete()
-        return JsonResponse({'message': 'issue was deleted successfully!'},
+        task.delete()
+        return JsonResponse({'message': 'task was deleted successfully!'},
                             status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET'])
-def issue_list_open(request):
-    issues = Issue.objects.filter(status="open")
+def task_list_open(request):
+    tasks = Task.objects.filter(status="open")
 
     if request.method == 'GET':
-        issues_serializer = IssueSerializer(issues, many=True)
-        return JsonResponse(issues_serializer.data, safe=False)
+        tasks_serializer = TaskSerializer(tasks, many=True)
+        return JsonResponse(tasks_serializer.data, safe=False)
