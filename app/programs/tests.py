e6ceece08d5a31c1759f86ebb8c135f70c97e4d7
Daniel Bonner <daniel@legacyretail.com>
2022-01-29 11:23:39 -0600

    program unit test

--- app/programs/tests.py
+++ app/programs/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
+import pytest
 
 # Create your tests here.
+def test_homepage_access():
+    url = reverse('home')
+    assert url == "/"
